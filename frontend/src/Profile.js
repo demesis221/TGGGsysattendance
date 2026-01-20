744fad9d4117ef6ec972e1d12a7daff978436331
demesis221 <rideouts199@gmail.com>
2026-01-20 11:35:49 +0800

    Fix: Remove unused imports in Profile.js

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { format } from 'date-fns';
-import { toZonedTime } from 'date-fns-tz';
 import Alert from './components/Alert';
 import { CardSkeleton } from './components/SkeletonLoader';
 
